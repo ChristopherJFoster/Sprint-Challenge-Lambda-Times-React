3d5bac7d35a788c98af80bbc54a05711a9d38d3b
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-02-22 11:28:32 -0500

    Started work on Content.js. Answer remaining 3 study questions.

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -1,16 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import Tabs from './Tabs';
-import Cards from './Cards';
+import Tabs from "./Tabs";
+import Cards from "./Cards";
 
 // Importing our tab and card data. No need to change anything here.
-import { tabData, cardData } from '../../data';
+import { tabData, cardData } from "../../data";
 
 export default class Content extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      selected: 'all',
+      selected: "all",
       tabs: [],
       cards: []
     };
@@ -48,7 +48,11 @@ export default class Content extends Component {
           `selectedTab` that includes the currently selected tab
           and `selectTabHandler` that includes the function to change the selected tab
         */}
-        <Tabs tabs={this.state.tabs} />
+        <Tabs
+          selectedTab={this.state.selected}
+          selectTabHandler={changeSelected}
+          tabs={this.state.tabs}
+        />
         <Cards cards={this.filterCards()} />
       </div>
     );
