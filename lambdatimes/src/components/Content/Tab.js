56a7f9d6fbbdbe95ae76e345d7280c7091a40669
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-02-22 12:21:43 -0500

    Misunderstood data structure earlier. Reworked PropTypes accordingly.

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,6 +1,7 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const Tab = ({ tab, selectTabHandler, selectedTab }) => {
+const Tab = ({ tab, selectedTab, selectTabHandler }) => {
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
       if they match, the className should be: 'tab active-tab', 
       if it is not it should just be 'tab'*/
@@ -10,7 +11,7 @@ const Tab = ({ tab, selectTabHandler, selectedTab }) => {
       onClick={
         /* Replace this dummy click handler function with your selectTabHandler function from props 
          you'll need to pass the `tab` in as an argument to this handler. */
-        e => selectTabHandler(e, tab)
+        e => selectTabHandler(e, tab.tab)
       }
     >
       {tab.toUpperCase()}
@@ -20,14 +21,9 @@ const Tab = ({ tab, selectTabHandler, selectedTab }) => {
 
 // Make sure you include PropTypes on your props.
 Tab.propTypes = {
-  tab: PropTypes.shape({
-    headline: PropTypes.string.isRequired,
-    tab: PropTypes.string.isRequired,
-    img: PropTypes.string.isRequired,
-    author: PropTypes.string.isRequired
-  }).isRequired,
-  selectTabHandler: PropTypes.func.isRequired,
-  selectedTab: PropTypes.func.isRequired
+  tab: PropTypes.string.isRequired,
+  selectedTab: PropTypes.string.isRequired,
+  selectTabHandler: PropTypes.func.isRequired
 };
 
 export default Tab;
--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,7 +1,8 @@
 import React from "react";
+import PropTypes from "prop-types";
 import Tab from "./Tab";
 
-const Tabs = ({ tabs, selectTabHandler, selectedTab }) => {
+const Tabs = ({ tabs, selectedTab, selectTabHandler }) => {
   return (
     <div className="tabs">
       <div className="topics">
@@ -9,9 +10,9 @@ const Tabs = ({ tabs, selectTabHandler, selectedTab }) => {
         {/* map over the tabs provided on your props, create a new Tab component for each one.
             give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
         {/* If the following gives the children warning, try Date.now() + index */}
-        {tabs.map(tab => (
+        {tabs.map((tab, index) => (
           <Tab
-            key={Date.now()}
+            key={Date.now() + index}
             tab={tab}
             selectTabHandler={selectTabHandler}
             selectedTab={selectedTab}
@@ -25,8 +26,8 @@ const Tabs = ({ tabs, selectTabHandler, selectedTab }) => {
 // Make sure to use PropTypes to validate your types!
 Tabs.propTypes = {
   tabs: PropTypes.array.isRequired,
-  selectTabHandler: PropTypes.func.isRequired,
-  selectedTab: PropTypes.func.isRequired
+  selectedTab: PropTypes.string.isRequired,
+  selectTabHandler: PropTypes.func.isRequired
 };
 
 export default Tabs;
